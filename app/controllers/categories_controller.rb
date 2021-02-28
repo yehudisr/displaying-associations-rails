e21dcfab16dd2f4a2978e965db678f2dbaf89c2b
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-02-28 14:37:04 -0500

    commit tree 5dd771e0f1b162ce0537a076986a2d0f6981e2c0

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+author1 = Author.create(name:"Penny", genre:"comical", bio:"a whole lot of nothing")
+author2 = Author.create(name:"Sally", genre:"history", bio:"grew up in brooklyn")
+author3 = Author.create(name:"John", genre:"tech", bio:"dreamed of this for a long time")
+
+category1 = Category.create(name:"tech")
+category2 = Category.create(name:"review")
+category3 = Category.create(name:"interview")
+
+Post.create(title:"How to", description:"a list of helpful tips", author: author1, category: category1)
+Post.create(title:"why not", description:"tell me the reason", author:author1, category:category3)
+Post.create(title:"sugary files", description:"discovery of sweet life hacks", author:author2, category:category3)
+Post.create(title:"now what?", description:"next steps to help you", author:author2, category:category3)
